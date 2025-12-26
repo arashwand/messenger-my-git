915045895df95b24aa2389e087a0e603b197145b
arashwand <jad1661@gmail.com>
2025-12-26 21:24:36 +0330

    ایجاد مدل

---
+++ DTOs/PrivateChatDto.cs
@@ -0,0 +1,10 @@
+﻿using Messenger.DTOs;
+
+namespace Messenger.DTOs
+{
+    public class PrivateChatDto
+    {
+        public Guid ConversationId { get; set; }
+        public IEnumerable<MessageDto> Messages { get; set; }
+    }
+}
